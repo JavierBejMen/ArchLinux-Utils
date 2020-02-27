70a7aacd32d9bca595e4ee3c42b19224a4fad3af
Javier Bejar Mendez <j.bejar.mendez@gmail.com>
2020-02-27 12:41:40 +0100

    updated

--- Scripts/Maintenance/AUR_upgrade.sh
+++ Scripts/Maintenance/AUR_upgrade.sh
@@ -21,40 +21,79 @@ specified path, 1 level recursion\n"
 
 #main functions
 process_dir(){
-  if [[ -d $1 ]]; then
-    printf "Updating package stored at ""$1""\n"
-    cd $1
-    remote_url=$(git config --get remote.origin.url)
-    if [ -z "$remote_url" ]; then
-      printf "[Error] can't find remote git url: remote.origin.url\n" >&2
-      exit 1
-    else
-      untracked_files=$(git ls-files --other --exclude-standard --directory | egrep -v '/$')
-
-      if [ ! -z "$untracked_files" ]; then
-        printf "Detected untracked files:\n""$TAB""$untracked_files\n"
-        printf "Do you wish to continue? [y/N]\n"
-        continue="N"
-        read continue
-        if [[ ${continue^^} == "N" || -z "$continue" ]]; then
-          printf "Aborted, no changes made\n"
-          exit 0
-        elif [[ ${continue^^} == "Y" ]]; then
-          #statements
-          printf "Pulling from ""$remote_url""\n"
-
-          # TODO - last here
-        else
-          printf "[Error] unknown response: ""$continue""\n" >&2
-          printf "Aborted, no changes made\n"
-          exit 1
-        fi
-      fi
-    fi
-  else
+  #check if directory exists
+  if [[ ! -d $1 ]]; then
     printf "[Error] can't open path: ""$1""\n" >&2
-    exit 1
+    return 1
   fi
+
+  printf "Updating package stored at ""$1""\n"
+  cd $1
+  #check if remote repository is available
+  remote_url=$(git config --get remote.origin.url)
+  if [ -z "$remote_url" ]; then
+    printf "[Error] can't find remote git url: remote.origin.url\n" >&2
+    return 1
+  fi
+
+  #check if updates are available
+  git_diff=$(git diff-index HEAD)
+  if [[ -z "$git_diff" ]]; then
+    printf "No updates available\n"
+    printf "Aborted, no changes were made"
+    return 0
+  fi
+
+  printf "Update available\n"
+
+  #check untracked files
+  # TODO HEREEE
+  untracked_files=$(git ls-files | egrep -v '/$')
+
+  if [ ! -z "$untracked_files" ]; then
+    printf "Detected untracked files:\n"
+    for f in $untracked_files
+    do
+      printf "$TAB""$f""\n"
+      if [[ f =~ .*.pkg ]]
+    done
+
+    printf "\nRemove untracked files? [y/N]\n"
+    continue="N"
+    read continue
+    if [[ ${continue^^} == "N" || -z "$continue" ]]; then
+      printf "Aborted, no changes made\n"
+      return 0
+    elif [[ ${continue^^} != "Y" ]]; then
+      printf "[Error] unknown response: ""$continue""\n" >&2
+      printf "Aborted, no changes made\n"
+      return 1
+    fi
+  fi
+
+  printf "Pulling from ""$remote_url""\n"
+  git pull $remote_url
+  if [[ $? == 1 ]]; then
+    printf "[Error] pulling operation returned fail code\n" >&2
+    printf "Check file status\n"
+    return 1
+  fi
+
+  printf "Building pakage\n"
+  makepkg
+  if [[ $? == 1 ]]; then
+    printf "[Error] makepkg operation returned fail code\n" >&2
+    printf "Check file status\n"
+    return 1
+  fi
+
+  package=$(ls | grep *pkg.tar.xz)
+  IFS=$'\n'
+
+  for pkg in $package
+  do
+    printf "$pkg""\n"
+  done
 }
 
 process_recursive_dir(){
@@ -62,7 +101,7 @@ process_recursive_dir(){
     echo ok
   else
     printf "[Error] can't open path: ""$1""\n" >&2
-    exit 1
+    return 1
   fi
 }
 
